7ad30fab26fd35885012d0fecec792c566619ea9
Popova Olesya <popovaolesya87@gmail.com>
2022-03-27 13:33:53 +0300

    Change Divide func

--- Calculator.java
+++ Calculator.java
@@ -3,20 +3,20 @@ package com.learningJava;
 import java.util.function.*;
 
 public class Calculator {
-    private Calculator() {
-    }
+    private Calculator() { }
 
     static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> x / y;
+    BinaryOperator<Integer> divide = (x, y) -> Integer.divideUnsigned(x,y);
     /*
-    Ошибка возникает при делении на ноль.
+    Ошибка возникает при делении на ноль:
+    BinaryOperator<Integer> divide = (x, y) -> x / y;
+
     Решение проблемы:
     BinaryOperator<Integer> divide = (x, y) -> Integer.divideUnsigned(x,y);
-    };
      */
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> Math.abs(x);
